7898af7e3783442a4591ee80430e59aa9409df5d
Daniel Jalaliev <66525925+DanielJalal@users.noreply.github.com>
2025-02-16 17:15:36 +0100

    add devblogs

---
+++ apartados/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var coll = document.getElementsByClassName("collapsible");
+    
+    for (var i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            content.style.display = content.style.display === "block" ? "none" : "block";
+        });
+    }
+});
+
+function playSoundAndRedirect() {
+    var audio = document.getElementById("madnessSound");
+    audio.play();
+    setTimeout(function () {
+        window.location.href = "sects/madness.html";
+    }, 1500);
+}
+
+function SoundAndRedirect() {
+    var audio = document.getElementById("mazikSound");
+    audio.play();
+    setTimeout(function () {
+        window.location.href = "https://open.spotify.com/user/312i6bqpxgzjpjjlheetnvyfmbbe";
+    }, 1500);
+}
